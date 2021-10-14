5408be4748664d141e00780a17fe3d54a693728b
Robert Ewald <robert.ewald@emtec.de>
2021-10-14 14:29:16 +0200

    follow redirects when downloading apk.

--- customize/install-novalauncher.sh
+++ customize/install-novalauncher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --output novalauncher.apk "http://teslacoilsw.com/tesladirect/download.pl?packageName=com.teslacoilsw.launcher"
+curl --location --output novalauncher.apk "http://teslacoilsw.com/tesladirect/download.pl?packageName=com.teslacoilsw.launcher"
 adb install novalauncher.apk
